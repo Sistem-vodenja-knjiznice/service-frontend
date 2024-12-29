7dd19f172d0646dcd296b9efa69075d2b7d3e8b6
Žan Mencigar <mencigarzan@gmail.com>
2024-12-29 23:33:12 +0100

    Added individual book view

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Login from "./pages/authentication/Login";
 import Register from "./pages/authentication/Register";
 import Books from "./pages/books/Books";
+import BooksView from "./pages/books/BooksView";
 import BooksCreate from "./pages/books/BooksCreate";
 import BooksEdit from "./pages/books/BooksEdit";
 import Borrows from "./pages/borrows/Borrows";
@@ -20,6 +21,7 @@ function App() {
                   <Route path="/register" Component={Register}/>
                   <Route path="/books" Component={Books}/>
                   <Route path="/books/create" Component={BooksCreate}/>
+                  <Route path="/books/:id" Component={BooksView}/>
                   <Route path="/books/:id/edit" Component={BooksEdit}/>
                   <Route path="/borrows" Component={Borrows}/>
                   <Route path="/users" Component={Users}/>
